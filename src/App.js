fa174afdcbc899a93c2b767a3227116091f5be84
WojtekObl <wojtek.obloza@gmail.com>
2021-04-20 22:34:21 +0200

    Aplikacja pokazujaca 3dniową pogodę.
    Połączono z API, modul wyszukowania miast, oraz pobierania progrnozy na 3 dni

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { fetchData, searchCity } from './fetchData.js';
 
-function App() {
+
+const App = () => {
+  const [cityName, setCityName] = useState("");
+  const [forecast, setForecast] = useState({});
+  const [showForecast, setShowForecast] = useState(false);
+  const [options, setOptions] = useState({});
+  const [open, setOpen] = useState(false)
+
+
+
+
+
+
+
+  const searchOptions = async () => {
+    try {
+      const data = await searchCity(cityName)
+      console.log(data);
+      setOptions(data)
+      // setOptions(Object.keys(data).map((key) => `${data[key].name}, ${data[key].country}`));
+      setOpen(true)
+
+    } catch (error) {
+      alert(error.message);
+      setOpen(false)
+    }
+  }
+
+
+
+  const handleForecast = async (city) => {
+    try {
+      const data = await fetchData(city)
+      console.log(data);
+
+      setForecast(data);
+      setShowForecast(true);
+
+    } catch (error) {
+      alert(error.message);
+    }
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app__tittle">3Day forecast app</h1>
+
+      <input
+        type="text"
+        className="app__search"
+        value={cityName}
+        onChange={(e) => setCityName(e.target.value)}
+      >
+      </input>
+      <button onClick={searchOptions}>searchCity</button>
+      {open &&
+        <div>
+          {Object.keys(options).map((key) =>
+            <button
+              onClick={() => {
+                handleForecast(options[key])
+                setOpen(false) 
+              }}
+              key={key}
+            >
+              {options[key].name}, {options[key].country}
+            </button>
+          )
+          }
+        </div>
+      }
+
+      {showForecast &&
+        <div className="app__forecast">
+          <div className="day">
+
+            <span>{forecast.daily[1].dt}</span>
+            <span>Weather: {forecast.daily[1].weather[0].main}</span>
+            <span>Temp: min {forecast.daily[1].temp.min}C max {forecast.daily[1].temp.max}C </span>
+            <span>Wind: {forecast.daily[1].wind_speed}km/h</span>
+
+          </div>
+          <div className="day">
+
+            <span>{forecast.daily[2].dt}</span>
+            <span>Weather: {forecast.daily[2].weather[0].main}</span>
+            <span>Temp: min {forecast.daily[2].temp.min}C max {forecast.daily[2].temp.max}C </span>
+            <span>Wind: {forecast.daily[2].wind_speed}km/h</span>
+
+          </div>
+          <div className="day">
+
+            <span>{forecast.daily[3].dt}</span>
+            <span>Weather: {forecast.daily[3].weather[0].main}</span>
+            <span>Temp: min {forecast.daily[3].temp.min}C max {forecast.daily[3].temp.max}C </span>
+            <span>Wind: {forecast.daily[3].wind_speed}km/h</span>
+
+          </div>
+
+        </div>
+      }
+
     </div>
   );
 }
---
+++ src/fetchData.js
@@ -0,0 +1,36 @@
+
+import axios from "axios";
+
+const apiKey = "151b7bd3af91874b71f10e22eebed35c";
+// const url = "https://api.openweathermap.org/data/2.5/forecast/";
+const url = "https://api.openweathermap.org/data/2.5/onecall";
+const urlSearch = "http://api.openweathermap.org/geo/1.0/direct"
+
+
+
+
+export const fetchData = async (city) => {
+    console.log(city)
+    const { data } = await axios.get(url, {
+        params: {
+            lat: city.lat,
+            lon: city.lon,
+            appid: apiKey,
+            exclude: 'hourly,current,minutely,alerts',
+            units: "metric",
+        }
+    });
+
+    return data;
+};
+
+export const searchCity = async (cityName) => {
+    const { data } = await axios.get(urlSearch, {
+        params: {
+            q: cityName,
+            appid: apiKey,
+            limit: 5,
+        }
+    });
+    return data;
+};
